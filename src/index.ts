eb09a40b47e2029cc1af63f41a98259a575d199c
Dusteleven <quinnkozik@gmail.com>
2025-04-03 22:39:21 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import fs from 'fs';
 
 const walletPath = process.env.WALLET || './wallets/wallet1.json';
 const reserve = Number(process.env.PRUDENT_RESERVE || 0.1);
+const waitMinMins = Number(process.env.MIN_MINS || 1);
+const waitMaxMins = Number(process.env.MAX_MINS || 5);
+const minSOL = Number(process.env.MIN_SOL || 0.1);
+const maxSOL = Number(process.env.MAX_SOL || 0.2);
 
 const keypair = Keypair.fromSecretKey(
   Uint8Array.from(JSON.parse(fs.readFileSync(walletPath, 'utf8')))
@@ -15,8 +19,8 @@ startIncrementalSell({
   connection,
   wallet: keypair,
   prudentReserveSOL: reserve,
-  waitMinMins: 1,
-  waitMaxMins: 5,
-  minSOL: 0.1,
-  maxSOL: 0.2
+  waitMinMins: waitMinMins,
+  waitMaxMins: waitMaxMins,
+  minSOL: minSOL,
+  maxSOL: maxSOL
 });
